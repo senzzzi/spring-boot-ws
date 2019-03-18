5805fc302bd2157346b2b4bb9cd47d4b992903fc
Bruno Rodrigues <brunorodrigues@Brunos-MacBook-Pro.local>
2019-03-18 11:21:08 +0000

    step-4

--- src/main/java/com/workshop/exampleapi/model/Brand.java
+++ src/main/java/com/workshop/exampleapi/model/Brand.java
@@ -1,8 +1,17 @@
 package com.workshop.exampleapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Brand {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String name;
 
 }
--- src/main/java/com/workshop/exampleapi/model/Car.java
+++ src/main/java/com/workshop/exampleapi/model/Car.java
@@ -1,9 +1,17 @@
 package com.workshop.exampleapi.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Car {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @ManyToOne
     private Brand brand;
+
     private String plate;
 
 }
