c85ce37071277b58ca8f00df49e7fe60bdcaf25f
Bruno Rodrigues <brunorodrigues@Brunos-MacBook-Pro.local>
2019-03-18 10:03:06 +0000

    Start.spring.io raw project

---
+++ src/main/java/com/workshop/exampleapi/ExampleapiApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.exampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleapiApplication.class, args);
+	}
+
+}
